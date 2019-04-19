80d725cb3b52e0076e122aca334960829eaa369e
Abdul Sami <sami-abdul@outlook.com>
2019-04-19 18:49:56 +0500

    Add package.json

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -5,5 +5,7 @@ var CertificateManager = artifacts.require("./CertificateManager.sol");
 module.exports = async function (deployer) {
   await deployer.deploy(CertificateStorage);
   await deployer.deploy(IssuerStorage);
-  await deployer.deploy(CertificateManager, CertificateStorage.address, IssuerStorage.address);
+  await deployer.deploy(CertificateManager, 'IBA Invent', CertificateStorage.address, IssuerStorage.address);
+
+  console.log("Certificate manager: " + CertificateManager.address);
 };
--- truffle-config.js
+++ truffle-config.js
@@ -1,13 +1,29 @@
-const path = require("path");
+let HDWalletProvider = require('truffle-hdwallet-provider');
+let mnemoic = "volcano memory stereo animal drift nuclear stairs dice venture mercy random sniff";
 
 module.exports = {
-  networks: {
-    development: {
-      host: "localhost",
-      port: 8545,
-      network_id: "*", // Match any network id
-      // gas: 2000000
+    networks: {
+        rinkeby: {
+            provider: function () {
+                return new HDWalletProvider(mnemoic, 'https://rinkeby.infura.io/v3/a2cecf6103164d56b460c9ac6283ae24')
+            },
+            network_id: 4,
+        },
+        ropsten: {
+            provider: function () {
+                return new HDWalletProvider(mnemoic, 'https://ropsten.infura.io/v3/a2cecf6103164d56b460c9ac6283ae24')
+            },
+            network_id: 3
+        },
+        development: {
+            host: "localhost",
+            port: 8545, // Using ganache as development network
+            network_id: "*"
+        }
+    },
+    solc: {
+        optimizer: {
+            enabled: true
+        }
     }
-  }
 };
-
